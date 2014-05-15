991a0c900d96926904e4ed81a849b086e9d82093
Andrei Oprea <andrei.br92@gmail.com>
2014-05-15 10:11:47 +0300

    Changed source & expected but tests still faill

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,37 +1,35 @@
 /**
-*
-* Copyright 2012 Adobe Systems Inc.;
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*
-*/
+ *
+ * Copyright 2012 Adobe Systems Inc.;
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 
 /*global module:false*/
 
 module.exports = function(grunt) {
 
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
 
         clean: {
-            release: ['css'],
+            release: ['css']
         },
 
         topcoat: {
             options: {
                 browsers: ['last 2 versions'],
-                namespace: 'topcoat',
-                license: grunt.file.read('test/fixtures/license.txt')
+                namespace: 'topcoat'
             },
             compile: {
                 files: [{
@@ -52,11 +50,10 @@ module.exports = function(grunt) {
                     destination: "demo",
                     template: "node_modules/topdoc-theme/",
                     templateData: {
-                      "title": "Topcoat",
-                      "subtitle": "CSS for clean and fast web apps",
-                      "homeURL": "http://topcoat.io",
-                      "debug": true
-                    },
+                        "title": "Topcoat",
+                        "subtitle": "CSS for clean and fast web apps",
+                        "homeURL": "http://topcoat.io"
+                    }
                 }
             }
         },
@@ -79,16 +76,16 @@ module.exports = function(grunt) {
 
     });
 
-    // These plugins provide necessary tasks.
-    grunt.loadNpmTasks('grunt-simple-mocha');
     grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-topcoat');
     grunt.loadNpmTasks('grunt-topdoc');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-simple-mocha');
 
-    grunt.registerTask('default', ['clean', 'build', 'test','release']);
+    grunt.registerTask('default', ['clean', 'build', 'test', 'release']);
     grunt.registerTask('build', ['topcoat']);
     grunt.registerTask('test', ['simplemocha']);
     grunt.registerTask('release', ['cssmin', 'topdoc']);
 
 };
+
